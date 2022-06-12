f92512314837d9108b936bb918b2aa0782beb4a5
Maycon Silva <jose.maycon.hermogenes.silva@gmail.com>
2022-06-12 17:50:19 -0300

    Ajustes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-import { API_CREDENTIALS } from '../credentials/api'
-
 export const API_ENDPOINTERS = {
   POST: {
     feedback: 'feedback'
@@ -9,8 +7,5 @@ export const API_ENDPOINTERS = {
 }
 
 export const API = axios.create({
-  baseURL: 'http://10.0.0.109:3333/',
-  headers: {
-    ...API_CREDENTIALS
-  }
+  baseURL: 'http://10.0.0.109:3333/'
 })
